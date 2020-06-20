9bb994273912f3dc11ca965e71e8357819c1ec47
Mahesh Patjoshi <maheshpatjoshi90@gmail.com>
2020-06-20 19:16:25 +0530

    Updates added on 20-06-2020

--- src/app.py
+++ src/app.py
@@ -23,6 +23,10 @@ def science():
 def gaming():
     return render_template('gaming.html')
 
+@app.route("/motorcycles")
+def motorcycles():
+    return render_template('motorcycles.html')
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
