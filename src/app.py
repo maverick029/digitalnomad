4962d8146c7e69b2875bc46e4fe15bf33e70a4cb
Mahesh Patjoshi <maheshpatjoshi90@gmail.com>
2020-08-02 18:15:01 +0530

    Updates added on 20-06-2020

--- src/app.py
+++ src/app.py
@@ -32,4 +32,4 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
